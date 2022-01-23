20dae3c209df41ba2d9a0ede3a781f0aa8724f56
Sandeepan-Das <sandeepandas2000@gmail.com>
2022-01-23 17:54:35 +0530

    parsing completed

--- server/Utills/Decompress/subDecompress.js
+++ server/Utills/Decompress/subDecompress.js
@@ -1,19 +1,21 @@
 const decompress = require("decompress");
 
-// const { unzipSubFiles } = require("./unZipSubFiles");
-var xmlFilePath = [];
 const subDecompress = async (dataFolder, folder, distFolder) => {
+  var xmlFilePath = [];
   const files = await decompress(`${dataFolder}/${folder}`, distFolder);
   for (const xmlFile of files) {
     if (xmlFile.type === "file") {
       if (xmlFile.path.endsWith(".zip")) {
+
         const newSubFile = await unzipSubFiles(distFolder, xmlFile.path);
         for (const subFile of newSubFile) {
-          // subFile.path = xmlFile.path.concat(subFile.path)  //optimize this
+          subFile.finalPath = `${xmlFile.path}/${subFile.path}`;
+
           files.push(subFile);
         }
       } else {
-        xmlFilePath.push(xmlFile.path);
+        if (!xmlFile.finalPath) xmlFile.finalPath = xmlFile.path;
+        xmlFilePath.push(xmlFile);
       }
     }
   }
